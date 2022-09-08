54e3dbef94333db0d5a3a27a00d33aeaa44cc2da
Luiz Filipe <luizfilipelgs@gmail.com>
2022-09-08 18:14:22 -0300

    req 4 corrigido

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import Login from '../pages/Login';
 import App from '../App';
 
 describe('Verifica o componente <Login />', () => {
   test('Se existe input de email e nome', () => {
-    renderWithRouterAndRedux(<App />);
+    renderWithRouterAndRedux(<Login />);
 
     const inputEmail = screen.getByTestId('input-gravatar-email');
     expect(inputEmail).toBeInTheDocument();
@@ -15,11 +16,12 @@ describe('Verifica o componente <Login />', () => {
     expect(inputname).toBeInTheDocument();
   });
 
-  test('O botão de logar "Play" será habilitado se for digitado um nome e um email em formato valido', () => {
-    renderWithRouterAndRedux(<App />);
+  test('O botão de logar "Play" será habilitado se for digitado um nome e um email em formato valido, e ao ser clicado deve redirecionar para rota /game', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+
     const inputname = screen.getByTestId('input-player-name');
     const inputEmail = screen.getByTestId('input-gravatar-email');
-    const btnLogin = screen.getByRole('button');
+    const btnLogin = screen.getByTestId('btn-play');
 
     expect(btnLogin).toBeInTheDocument();
     expect(btnLogin).toBeDisabled();
@@ -31,5 +33,10 @@ describe('Verifica o componente <Login />', () => {
     userEvent.type(inputname, 'joao');
     userEvent.type(inputEmail, 'teste@teste.com');
     expect(btnLogin).toBeEnabled();
+
+    userEvent.click(btnLogin);
+    expect(history.location.pathname).toBe('/game');
   });
+
+ 
 });
