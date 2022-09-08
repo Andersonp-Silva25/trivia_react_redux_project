dc401b5c12a7ef7e34bd83fd9ae4de6915b38756
Luiz Filipe <luizfilipelgs@gmail.com>
2022-09-08 20:15:14 -0300

    req 13 ok- aguardando score do online store para aprovação

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Feedback extends Component {
+  render() {
+    const TRES = 3;
+    const { score } = this.props;
+    return (
+      <div>
+        <h2>Feedback</h2>
+        { score < TRES && <p data-testid="feedback-text">Could be better...</p> }
+        { score >= TRES && <p data-testid="feedback-text">Well Done!</p> }
+      </div>
+    );
+  }
+}
+
+Feedback.propTypes = {
+  score: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  score: state.score,
+});
+
+export default connect(mapStateToProps)(Feedback);
