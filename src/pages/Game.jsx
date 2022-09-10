3e02d19a1101e164db53d2351689ebb1567b210c
Joao Guilherme <jguilherme.marcos@gmail.com>
2022-09-10 16:28:58 -0300

    req 7 e 8

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -13,7 +13,11 @@ class Game extends React.Component {
       loading: true,
       redirect: false,
       position: Math.floor(Math.random() * positions),
+      timer: 30,
+      timeout: false,
     };
+    this.handleAnswerClick = this.handleAnswerClick.bind(this);
+    this.updateTimer = this.updateTimer.bind(this);
   }
 
   async componentDidMount() {
@@ -21,17 +25,48 @@ class Game extends React.Component {
     const gameURL = `https://opentdb.com/api.php?amount=5&token=${token}`;
     const data = await fetchGameApi(gameURL);
     const badToken = 3;
-    console.log(data);
     if (data.response_code === badToken) {
       localStorage.removeItem('token');
       this.setState({ redirect: true });
     } else {
       this.setState({ data, loading: false });
     }
+    const interval = 1000;
+    setInterval(this.updateTimer, interval);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { currentQuestion } = this.state;
+    // console.log(prevState);
+    // console.log(currentQuestion);
+  }
+
+  handleAnswerClick() {
+    document.querySelectorAll('#wrong-answer').forEach((elem) => {
+      elem.classList.add('wrong-answer');
+    });
+    document.querySelectorAll('#right-answer').forEach((elem) => {
+      elem.classList.add('right-answer');
+    });
+    const { currentQuestion } = this.state;
+    const nextQuestion = currentQuestion + 1;
+    this.setState({ currentQuestion: nextQuestion });
+  }
+
+  updateTimer() {
+    let { timer } = this.state;
+    timer -= 1;
+    if (timer >= 0) {
+      this.setState({ timer });
+    }
+    if (timer === 0) {
+      this.setState({ timeout: true });
+    }
   }
 
   render() {
-    const { data, currentQuestion, loading, redirect, position } = this.state;
+    const { data, currentQuestion, loading, redirect, position, timer,
+      timeout } = this.state;
     const answerPosition = 6;
     return (
       <div>
@@ -42,6 +77,7 @@ class Game extends React.Component {
             ? (<div>Loading...</div>)
             : (
               <div>
+                <p>{timer}</p>
                 <p
                   data-testid="question-category"
                 >
@@ -59,6 +95,9 @@ class Game extends React.Component {
                         <button
                           type="button"
                           data-testid="correct-answer"
+                          id="right-answer"
+                          onClick={ this.handleAnswerClick }
+                          disabled={ timeout }
                         >
                           {data.results[currentQuestion].correct_answer}
                         </button>
@@ -70,6 +109,9 @@ class Game extends React.Component {
                       type="button"
                       key={ index }
                       data-testid={ `wrong-answer-${index}` }
+                      id="wrong-answer"
+                      onClick={ this.handleAnswerClick }
+                      disabled={ timeout }
                     >
                       {elem}
 
@@ -81,6 +123,9 @@ class Game extends React.Component {
                         <button
                           type="button"
                           data-testid="correct-answer"
+                          id="right-answer"
+                          onClick={ this.handleAnswerClick }
+                          disabled={ timeout }
                         >
                           {data.results[currentQuestion].correct_answer}
                         </button>
